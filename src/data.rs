496c8b76a26878bb144b2ad8998909a0285f56e0
WolfkillArcadia <WolfkillArcadia@gmail.com>
2021-09-11 16:06:01 -0400

    Clarified datatype

--- src/data.rs
+++ src/data.rs
@@ -11,7 +11,7 @@ use serde::{Deserialize, Serialize};
 macro_rules! retrieve_data {
     ($message:expr, $data_type:ident) => {{
         let data = match &$message.data {
-            Data::$data_type(ref v) => v,
+            esm_message::Data::$data_type(ref v) => v,
             data => panic!("Unexpected data type {:?}. Expected: {}.", data, stringify!($data_type))
         };
 
