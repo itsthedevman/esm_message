06a8f2dd13723af8a526a4b32825170f877a5d75
WolfkillArcadia <WolfkillArcadia@gmail.com>
2022-04-21 18:12:14 -0400

    Implemented from_arma for error

--- message/src/error.rs
+++ message/src/error.rs
@@ -1,4 +1,5 @@
 use serde::{Deserialize, Serialize};
+use serde_json::Value as JSONValue;
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Error {
@@ -17,6 +18,39 @@ impl Error {
             error_content,
         }
     }
+
+    pub fn from_arma(input: String) -> Result<Vec<Self>, String> {
+        let input: JSONValue = match serde_json::from_str(&input) {
+            Ok(v) => v,
+            Err(e) => {
+                return Err(format!(
+                    "[esm_message::error::from_arma] Failed to convert input into JSONValue. Reason: {e}. Input: {input:?}"
+                ))
+            }
+        };
+
+        let errors = crate::parser::validate_content(&input);
+        let error_array = match errors.as_array() {
+            Some(e) => e,
+            None => return Err(format!("[esm_message::error::from_arma] Failed to convert validated errors to array. Errors: \"{errors:?}\"")),
+        };
+
+        let mut errors: Vec<Error> = Vec::new();
+        for error in error_array {
+            let error = crate::parser::validate_content(error);
+            let json = match serde_json::to_string(&error) {
+                Ok(j) => j,
+                Err(e) => return Err(format!("[esm_message::error::from_arma] Failed to convert to final JSON. Reason: {e}. Error: \"{error:?}\"")),
+            };
+
+            match serde_json::from_str(&json) {
+                Ok(e) => errors.push(e),
+                Err(e) => return Err(format!("[esm_message::error::from_arma] Failed to convert to Error. Reason: {e}. Error: \"{error:?}\" ")),
+            };
+        }
+
+        Ok(errors)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
@@ -27,4 +61,40 @@ pub enum ErrorType {
 
     // Treats the error_message as a normal string
     Message,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use arma_rs::IntoArma;
+    use serde_json::json;
+
+    #[test]
+    fn it_converts_to_error_vec() {
+        let input = json!([
+            json!([json!(["type", "content"]), json!(["code", "SOME_CODE"]),]),
+            json!([
+                json!(["type", "content"]),
+                json!(["message", "This is some message"])
+            ])
+        ])
+        .to_arma()
+        .to_string();
+
+        let result: Result<Vec<Error>, String> = Error::from_arma(input);
+
+        assert_eq!(
+            result.unwrap(),
+            vec![
+                Error {
+                    error_type: ErrorType::Code,
+                    error_content: "SOME_CODE".to_string()
+                },
+                Error {
+                    error_type: ErrorType::Message,
+                    error_content: "This is some message".to_string()
+                }
+            ]
+        );
+    }
 }
